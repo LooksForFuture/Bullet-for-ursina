dc1c13627827356314d7141932274e62a1cfaf82
Mohammad Armin Niknami <56917818+LooksForFuture@users.noreply.github.com>
2021-05-06 19:27:30 +0430

    Added CylinderCollider

--- collider.py
+++ collider.py
@@ -1,11 +1,11 @@
 from statistics import mean
 from ursina import Ursina, Entity, Vec3, Cylinder
 from panda3d.bullet import (
-    BulletRigidBodyNode, BulletGhostNode, BulletSphereShape, BulletBoxShape
+    BulletRigidBodyNode, BulletGhostNode, BulletSphereShape, BulletBoxShape, BulletCylinderShape
     )
 
 class Collider:
-    def __init__(self, world:Ursina, entity:Entity, shape, name, mass, ghost):
+    def __init__(self, world:Ursina, entity:Entity, shape, name, rotation, mass, ghost):
         self.world = world
         if ghost:
             self.node = BulletGhostNode(name)
@@ -16,7 +16,12 @@ class Collider:
         self.node.addShape(shape)
         self.np = world.render.attachNewNode(self.node)
         self.np.setPos(entity.x, entity.y, entity.z)
-        self.np.setHpr(entity.getHpr())
+
+        if None in rotation:
+            hpr = entity.getHpr()
+            for x in range(len(hpr)):
+                rotation[x] = hpr[x]
+        self.np.setHpr(Vec3(*rotation))
 
         if ghost:
             world.attachGhost(self.node)
@@ -38,34 +43,34 @@ class Collider:
         self.np.setCollideMask(mask)
     
     @property
-    def angularDamping(self):
+    def angular_damping(self):
         return self.node.getAngularDamping()
     
-    @angularDamping.setter
-    def angularDamping(self, value):
+    @angular_damping.setter
+    def angular_damping(self, value):
         self.node.setAngularDamping(value)
     
     @property
-    def angularFactor(self):
+    def angular_factor(self):
         return self.node.getAngularFactor()
     
-    @angularFactor.setter
-    def angularFactor(self, value):
+    @angular_factor.setter
+    def angular_factor(self, value):
         self.node.setAngularFactor(value)
     
     @property
-    def angularSleepThreshold(self):
+    def angular_sleep_threshold(self):
         return self.node.getAngularSleepThreshold()
     
-    @angularSleepThreshold.setter
-    def angularSleepThreshold(self, value):
+    @angular_sleep_threshold.setter
+    def angular_sleep_threshold(self, value):
         self.node.setAngularSleepThreshold(value)
     
     @property
-    def angularVelocity(self):
+    def angular_velocity(self):
         return self.node.getAngularVelocity()
     
-    @angularVelocity.setter
+    @angular_velocity.setter
     def angularVelocity(self, value):
         self.node.setAngularVelocity(value)
     
@@ -86,35 +91,35 @@ class Collider:
         self.node.setInertia(value)
     
     @property
-    def linearDamping(self):
+    def linear_damping(self):
         return self.node.getLinearDamping()
     
-    @linearDamping.setter
-    def linearDamping(self, value):
+    @linear_damping.setter
+    def linear_damping(self, value):
         self.node.setLinearDamping(value)
     
     @property
-    def linearFactor(self):
+    def linear_factor(self):
         return self.node.getLinearFactor()
     
-    @linearFactor.setter
-    def linearFactor(self, value):
+    @linear_factor.setter
+    def linear_factor(self, value):
         self.node.setLinearFactor(value)
     
     @property
-    def linearSleepThreshold(self):
+    def linear_sleep_threshold(self):
         return self.node.getLinearSleepThreshold()
     
-    @linearSleepThreshold.setter
-    def linearSleepThreshold(self, value):
+    @linear_sleep_threshold.setter
+    def linear_sleep_threshold(self, value):
         self.node.setLinearSleepThreshold(value)
     
     @property
-    def linearVelocity(self):
+    def linear_velocity(self):
         return self.node.getLinearVelocity()
     
-    @linearVelocity.setter
-    def linearVelocity(self, value):
+    @linear_velocity.setter
+    def linear_velocity(self, value):
         self.node.setLinearVelocity(value)
     
     @property
@@ -145,42 +150,71 @@ class Collider:
     def total_torque(self):
         return self.node.total_torque
     
-    def applyCentralForce(self, force):
+    def apply_central_force(self, force):
         self.node.applyCentralForce(force)
     
-    def applyCentralImpulse(self, impulse):
+    def apply_central_impulse(self, impulse):
         self.node.applyCentralImpulse(impulse)
     
-    def applyForce(self, force, pos):
+    def apply_force(self, force, pos):
         self.node.applyForce(force, pos)
     
-    def applyImpulse(self, impulse, pos):
+    def apply_impulse(self, impulse, pos):
         self.node.applyImpulse(impulse, pos)
     
-    def applyTorque(self, torque):
+    def apply_torque(self, torque):
         self.node.applyTorque(torque)
     
-    def applyTorqueImpulse(self, torque):
+    def apply_torque_impulse(self, torque):
         self.node.applyTorqueImpulse(torque)
     
-    def clearForces(self):
+    def clear_forces(self):
         self.node.clearForces()
     
-    def pickDirtyFlag(self):
+    def pick_dirty_flag(self):
         return self.node.pickDirtyFlag()
 
 class SphereCollider(Collider):
-    def __init__(self, world:Ursina, entity:Entity, scale=0.5, mass=0, ghost=False) -> None:
-        model = str(entity.model).split('/')[-1]
-        if model == 'sphere':
-            scale = mean(entity.scale)/2
+    def __init__(
+        self, world:Ursina, entity:Entity,
+        rotation=[None, None, None], scale=None,
+        mass=0, ghost=False
+        ) -> None:
 
-        super().__init__(world, entity, BulletSphereShape(scale), 'sphere', mass, ghost)
+        if scale == None:
+            model = str(entity.model).split('/')[-1]
+            if model == 'sphere':
+                scale = mean(entity.scale)/2
+            else:
+                scale = 0.5
+
+        super().__init__(world, entity, BulletSphereShape(scale), 'sphere', rotation, mass, ghost)
 
 class BoxCollider(Collider):
-    def __init__(self, world:Ursina, entity:Entity, scale=(0.5, 0.5, 0.5), mass=0, ghost=False) -> None:
-        model = str(entity.model).split('/')[-1]
-        if model == 'cube':
-            scale = (x/2 for x in entity.scale)
+    def __init__(
+        self, world:Ursina, entity:Entity,
+        rotation=[None, None, None], scale=[None, None, None],
+        mass=0, ghost=False
+        ) -> None:
+        
+        if None in scale:
+            for x in range(len(entity.scale)):
+                scale[x] = entity.scale[x]/2
 
-        super().__init__(world, entity, BulletBoxShape(Vec3(*scale)), 'box', mass, ghost)
+        super().__init__(world, entity, BulletBoxShape(Vec3(*scale)), 'box', rotation, mass, ghost)
+
+class CylinderCollider(Collider):
+    def __init__(
+        self, world:Ursina, entity:Entity,
+        rotation=[None, None, None], radius=None, height=None,
+        mass=0, ghost=False
+        ):
+
+        if radius==None or height==None:
+            if type(entity.model) == Cylinder:
+                radius=entity.model.radius
+                height=entity.model.height
+            else:
+                radius=.5
+                height=1
+        super().__init__(world, entity, BulletCylinderShape(radius, height, 1), 'cylinder', rotation, mass, ghost)
