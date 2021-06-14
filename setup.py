e0556599b5489f867bd7b5837368fdf692f588f7
Mohammad Armin Niknami <56917818+LooksForFuture@users.noreply.github.com>
2021-06-14 16:21:30 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='physics3d',
+      version='1.0',
+      description='Bullet physics for ursina',
+      author='Looks For Future',
+      packages=['physics3d'],
+      package_dir={'physics3d':'physics3d'}
+     )
