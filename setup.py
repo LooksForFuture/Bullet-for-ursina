ae394f7306a1281ef290502d1a343b25f450035f
Mohammad Armin Niknami <56917818+LooksForFuture@users.noreply.github.com>
2021-06-15 09:54:03 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(name='physics3d',
       version='1.0',
       description='Bullet physics for ursina',
-      author='Looks For Future',
+      author='Mohammad Armin Niknami',
+      author_email='mohammad.armin.niknami@gmail.com',
+      url='https://github.com/LooksForFuture/Bullet-for-ursina',
       packages=['physics3d'],
       package_dir={'physics3d':'physics3d'}
      )
