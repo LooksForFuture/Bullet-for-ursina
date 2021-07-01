631faff16db06bb4e5479ef2c25451a104273216
Mohammad Armin Niknami <56917818+LooksForFuture@users.noreply.github.com>
2021-07-01 17:09:04 +0430

    Better API

--- physics3d/character_controller.py
+++ physics3d/character_controller.py
@@ -1,28 +1,25 @@
 from ursina import Entity, Vec3, application
-from panda3d.core import BitMask32
 from panda3d.bullet import BulletWorld, BulletCapsuleShape, BulletCharacterControllerNode
 
-class CharacterController:
+class CharacterController(BulletCharacterControllerNode):
     def __init__(self, world:BulletWorld, entity:Entity, radius=1, height=2, name='Player', **opts) -> None:
-        self.__shape = BulletCapsuleShape(radius/2, height/2, 1)
-        self.node = BulletCharacterControllerNode(self.__shape, radius/2, name)
-        self.np = application.base.render.attachNewNode(self.node)
+        super().__init__(BulletCapsuleShape(radius/2, height/2, 1), radius/2, name)
+        self.np = application.base.render.attachNewNode(self)
 
         if entity.parent:
             self.np.reparent_to(entity.parent)
         
-        rotation = [None, None, None]
+        rotation = Vec3(0, 0, 0)
         if None in rotation:
             hpr = entity.getHpr()
             for x in range(len(hpr)):
                 rotation[x] = hpr[x]
-        self.np.setHpr(Vec3(*rotation))
+        self.np.setHpr(rotation)
         
         self.np.setPos(entity.x, entity.y, entity.z)
         entity.reparent_to(self.np)
 
-        self.np.setCollideMask(BitMask32.allOn())
-        world.attachCharacter(self.node)
+        world.attachCharacter(self)
 
         self.__fall_speed = None
         self.__jump_speed = None
@@ -32,23 +29,17 @@ class CharacterController:
             setattr(self, x, opts[x])
     
     def jump(self):
-        self.node.doJump()
+        self.doJump()
     
     def move(self, vel:Vec3, is_local:bool):
-        self.node.setLinearMovement(vel, is_local)
+        self.setLinearMovement(vel, is_local)
     
     def rotate(self, omega:float):
-        self.node.setAngularMovement(omega)
+        self.setAngularMovement(omega)
     
-    def set_max_slope(self, slope:float):
-        self.node.setMaxSlope(slope)
-    
-    def set_use_ghost_sweep_test(self, val:bool):
-        self.node.setUseGhostSweepTest(val)
-
     @property
     def can_jump(self):
-        return self.node.canJump()
+        return self.canJump()
     
     @property
     def fall_speed(self):
@@ -57,15 +48,15 @@ class CharacterController:
     @fall_speed.setter
     def fall_speed(self, speed:float):
         self.__fall_speed = speed
-        self.node.setFallSpeed(speed)
+        self.setFallSpeed(speed)
     
     @property
     def gravity(self):
-        return self.node.gravity
+        return self.gravity
     
     @gravity.setter
     def gravity(self, grav:float):
-        self.node.setGravity(grav)
+        self.setGravity(grav)
     
     @property
     def jump_speed(self):
@@ -74,7 +65,7 @@ class CharacterController:
     @jump_speed.setter
     def jump_speed(self, speed:float):
         self.__jump_speed = speed
-        self.node.setJumpSpeed(speed)
+        self.setJumpSpeed(speed)
     
     @property
     def max_jump_height(self):
@@ -83,12 +74,8 @@ class CharacterController:
     @max_jump_height.setter
     def max_jump_height(self, max_jump_height:float):
         self.__max_jump_height = max_jump_height
-        self.node.setMaxJumpHeight(max_jump_height)
+        self.setMaxJumpHeight(max_jump_height)
     
     @property
     def on_ground(self):
-        return self.node.isOnGround()
-    
-    @property
-    def shape(self):
-        return self.__shape
+        return self.isOnGround()
